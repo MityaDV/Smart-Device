d25ee4afbf5cbe29c936441af1fe3054942f5b42
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-12-26 18:25:34 +0200

    проверки js

--- source/js/main.js
+++ source/js/main.js
@@ -2,17 +2,21 @@
 
 var buttonOpenPopup = document.querySelector('.btn--call');
 var modal = document.querySelector('.modal-js');
-var buttonClosePopup = modal.querySelector('.modal__button-close');
-var modalForm = modal.querySelector('.modal__form');
+if (modal) {
+  var buttonClosePopup = modal.querySelector('.modal__button-close');
+  var modalForm = modal.querySelector('.modal__form');
+  var nameUser = modal.querySelector('[name=username]');
+  var phoneUser = modal.querySelector('[name=userphone]');
+  var questionUser = modal.querySelector('[name=userquestion]');
+}
 var pageOverlay = document.querySelector('.overlay');
-var nameUser = modal.querySelector('[name=username]');
-var phoneUser = modal.querySelector('[name=userphone]');
-var questionUser = modal.querySelector('[name=userquestion]');
 var footer = document.querySelector('.footer');
-var footerNav = footer.querySelector('.footer__navigation');
-var footerContact = footer.querySelector('.footer__contact');
-var footerToggleNav = footer.querySelector('.footer__toggle--navigation');
-var footerToggleContact = footer.querySelector('.footer__toggle--contact');
+if (footer) {
+  var footerNav = footer.querySelector('.footer__navigation');
+  var footerContact = footer.querySelector('.footer__contact');
+  var footerToggleNav = footer.querySelector('.footer__toggle--navigation');
+  var footerToggleContact = footer.querySelector('.footer__toggle--contact');
+}
 var btnScrollDown = document.querySelector('.promo__scroll-button');
 var isStorageSupport = true;
 var storage = '';
@@ -20,27 +24,32 @@ var storagePhone = '';
 var storageText = '';
 var ESC_KEYCODE = 27;
 
-footerToggleNav.addEventListener('click', function () {
-  if (footerNav.classList.contains('footer__navigation--closed')) {
-    footerNav.classList.remove('footer__navigation--closed');
-    footerNav.classList.add('footer__navigation--opened');
-  } else {
-    footerNav.classList.add('footer__navigation--closed');
-    footerNav.classList.remove('footer__navigation--opened');
-  }
-});
+if (footerToggleNav) {
+  footerToggleNav.addEventListener('click', function () {
+    if (footerNav.classList.contains('footer__navigation--closed')) {
+      footerNav.classList.remove('footer__navigation--closed');
+      footerNav.classList.add('footer__navigation--opened');
+    } else {
+      footerNav.classList.add('footer__navigation--closed');
+      footerNav.classList.remove('footer__navigation--opened');
+    }
+  });
+}
 
-footerToggleContact.addEventListener('click', function () {
-  if (footerContact.classList.contains('footer__contact--closed')) {
-    footerContact.classList.remove('footer__contact--closed');
-    footerContact.classList.add('footer__contact--opened');
-  } else {
-    footerContact.classList.add('footer__contact--closed');
-    footerContact.classList.remove('footer__contact--opened');
-  }
-});
+if (footerToggleContact) {
+  footerToggleContact.addEventListener('click', function () {
+    if (footerContact.classList.contains('footer__contact--closed')) {
+      footerContact.classList.remove('footer__contact--closed');
+      footerContact.classList.add('footer__contact--opened');
+    } else {
+      footerContact.classList.add('footer__contact--closed');
+      footerContact.classList.remove('footer__contact--opened');
+    }
+  });
+}
 
 // сохраняем значения полей в localStorage
+
 try {
   storage = localStorage.getItem('nameUser');
   storagePhone = localStorage.getItem('phoneUser');
@@ -50,6 +59,7 @@ try {
 }
 
 // утилита для обработки клавиатурных событий
+
 var isEscEvent = function (evt, action) {
   if (evt.keyCode === ESC_KEYCODE) {
     evt.preventDefault();
@@ -58,11 +68,13 @@ var isEscEvent = function (evt, action) {
 };
 
 // обработчик нажатия ESC
+
 var onPopupEscPress = function (evt) {
   isEscEvent(evt, closePopup);
 };
 
 // обработчик нажатия на overlay
+
 var onCLickOverlay = function () {
   if (modal.classList.contains('modal__open') && pageOverlay.classList.contains('overlay__open')) {
     modal.classList.remove('modal__open');
@@ -72,26 +84,30 @@ var onCLickOverlay = function () {
 };
 
 // логика открытия popup
-var openPopup = function (evt) {
-  evt.preventDefault();
+if (modal) {
+  var openPopup = function (evt) {
+    evt.preventDefault();
 
-  if (storage) {
-    nameUser.value = storage;
-    phoneUser.value = storagePhone;
-    questionUser.value = storageText;
-    questionUser.focus();
-  } else {
-    nameUser.focus();
-  }
+    if (storage) {
+      nameUser.value = storage;
+      phoneUser.value = storagePhone;
+      questionUser.value = storageText;
+      questionUser.focus();
+    } else {
+      nameUser.focus();
+    }
 
-  modal.classList.add('modal__open');
-  pageOverlay.classList.add('overlay__open');
+    modal.classList.add('modal__open');
+    pageOverlay.classList.add('overlay__open');
 
-  pageOverlay.addEventListener('click', onCLickOverlay);
-  document.addEventListener('keydown', onPopupEscPress);
-};
+    pageOverlay.addEventListener('click', onCLickOverlay);
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+}
+
 
 // логика закрытия popup
+
 var closePopup = function () {
 
   if (modal.classList.contains('modal__open') && pageOverlay.classList.contains('overlay__open')) {
@@ -105,11 +121,13 @@ var closePopup = function () {
 };
 
 // логика ошибки при вводе данных
+
 var errorPopup = function () {
   modal.classList.add('modal__error');
 };
 
 // логика валидации формы popup
+
 var formValidation = function (evt) {
   if (!nameUser.value || !phoneUser.value || !questionUser.value) {
     evt.preventDefault();
@@ -123,9 +141,16 @@ var formValidation = function (evt) {
 };
 
 // навешиваем события на элементы
-buttonOpenPopup.addEventListener('click', openPopup);
-buttonClosePopup.addEventListener('click', closePopup);
-modalForm.addEventListener('submit', formValidation);
+if (buttonOpenPopup) {
+  buttonOpenPopup.addEventListener('click', openPopup);
+}
+if (buttonClosePopup) {
+  buttonClosePopup.addEventListener('click', closePopup);
+}
+
+if (modalForm) {
+  modalForm.addEventListener('submit', formValidation);
+}
 
 // прокрутка к блоку преимуществ
 
@@ -141,8 +166,9 @@ function scrollDown() {
     }
   })();
 }
-
-btnScrollDown.addEventListener('click', scrollDown);
+if (btnScrollDown) {
+  btnScrollDown.addEventListener('click', scrollDown);
+}
 
 // прокрутка к форме
 
